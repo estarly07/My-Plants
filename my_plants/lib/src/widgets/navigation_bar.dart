62fbb965f05b7cafe27bc518d7154eb18d7cf15a
Estarly Amaya <estarlyjesusamaya@gmail.com>
2021-11-20 18:58:06 -0500

    CREATION AND IMPLEMENTATION
    - Flutter svg library was implemented
    - NavigatorBar widget was created

---
+++ my_plants/lib/src/widgets/navigation_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      height: size.width * 0.2,
+      width: size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _ItemsNavigationBar(),
+          _ItemsNavigationBar(),
+          _ItemsNavigationBar(),
+          _ItemsNavigationBar(),
+        ],
+      ),
+      decoration: const BoxDecoration(
+          color: Color(0xff228B22),
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50), topRight: Radius.circular(50))),
+    );
+  }
+}
+
+class _ItemsNavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.all(5),
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25), color: Colors.white),
+        ),
+        Icon(
+          Icons.home,
+          size: 30,
+          color: Colors.white,
+        ),
+      ],
+    );
+  }
+}
---
+++ my_plants/lib/src/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:my_plants/src/widgets/navigation_bar.dart';
